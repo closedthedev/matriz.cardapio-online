5bc2e09f94502cb4d7d4c08c9dd23d828bec11df
closedthedev <104802109+closedthedev@users.noreply.github.com>
2024-08-19 23:41:53 -0300

    att

--- docs/script.js
+++ docs/script.js
@@ -103,6 +103,43 @@ for(var i = 0; i < botoesAdicionar.length; i++) {
 }
 
 
+// Seleciona todos os elementos com a classe 'share' e adiciona o evento de clique a cada um
+var buttons = document.getElementsByClassName('share');
+
+// Itera sobre todos os elementos com a classe 'share'
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+        var button = this;
+        button.textContent = 'Adicionado ao carrinho'; // Altera o texto do botão para "Adicionado"
+        button.classList.add('changed'); // Adiciona a classe para alterar a cor do botão
+
+        setTimeout(function() {
+            button.textContent = 'Adicionar'; // Retorna o texto do botão para "Clique aqui"
+            button.classList.remove('changed'); // Remove a classe após 3 segundos
+        }, 3000); // 3000 milissegundos = 3 segundos
+    });
+}
+var botoesAdicionar = document.getElementsByClassName('share');
+for(var i = 0; i < botoesAdicionar.length; i++) {
+    botoesAdicionar[i].addEventListener('click', function(event) {
+        event.stopPropagation();
+    });
+}
+
+// script.js
+document.addEventListener('DOMContentLoaded', (event) => {
+    const messageElement = document.getElementById('message');
+    
+    // Define o tempo em milissegundos (4000 ms = 4 segundos)
+    const tempoDesaparecer = 4100; 
+
+    // Define um temporizador para esconder o parágrafo após o tempo especificado
+    setTimeout(() => {
+        messageElement.style.display = 'none';
+    }, tempoDesaparecer);
+});
+
+
 
 window.addEventListener('scroll', function() {
     var stickyElement = document.getElementById('stickyElement');
@@ -140,3 +177,99 @@ function openItemDetails(name, price, image, description) {
         }
     };
 }
+
+// Abre o popup de informações
+document.getElementById('infoPopup').addEventListener('click', function() {
+    document.getElementById('infoModal').style.display = 'block';
+});
+
+// Fecha o popup de informações
+document.getElementById('closeInfo').addEventListener('click', function() {
+    document.getElementById('infoModal').style.display = 'none';
+});
+
+// Fecha o popup se o usuário clicar fora do conteúdo do popup
+window.addEventListener('click', function(event) {
+    if (event.target == document.getElementById('infoModal')) {
+        document.getElementById('infoModal').style.display = 'none';
+    }
+});
+
+
+// script.js
+document.getElementById('copyImage').addEventListener('click', function() {
+    // Obtém a URL atual
+    const url = window.location.href;
+    
+    // Cria um elemento temporário de input
+    const tempInput = document.createElement('input');
+    tempInput.style.position = 'absolute';
+    tempInput.style.left = '-9999px';
+    tempInput.value = url;
+    
+    // Adiciona o input ao corpo do documento
+    document.body.appendChild(tempInput);
+    
+    // Seleciona o conteúdo do input
+    tempInput.select();
+    tempInput.setSelectionRange(0, 99999); // Para dispositivos móveis
+
+    // Copia o conteúdo selecionado para a área de transferência
+    document.execCommand('copy');
+    
+    // Remove o input temporário
+    document.body.removeChild(tempInput);
+
+    // Mostra uma mensagem de confirmação
+    const messageElement = document.getElementById('message');
+    messageElement.textContent = 'URL copiada para a área de transferência!';
+});
+
+// Lista de caminhos para as imagens
+var imagens = [
+    "assets/brahma.jpg",
+    "assets/burgerfrango.png",
+    "assets/cheeseburger.png",
+    "assets/duplobacon.png",
+    "assets/quartobacon.png",
+    "assets/triplobacon.png",
+    "assets/burgersalada.png",
+    "assets/cocacola.png",
+    "assets/guarana2lt.png",
+    "assets/heineken.png",
+    "assets/icons8-cart-64.png",
+    "assets/icons8-close-48.png",
+    "assets/imagem1.icons8-info-50.png",
+    "assets/imagem2.icons8-share-48.png",
+    "assets/imagem3.mozedlogo.png",
+    "assets/mozerd_capa.png",
+    "assets/pepsi1.png",
+    "assets/pizzacogumelo.png",
+    "assets/pizza4queijos.png",
+    "assets/pizzamargueritha.png",
+    "assets/pizzabacon.png",
+    "assets/pizzafrangocatupiry.png",
+    "assets/pizzacalabresa.png",
+    "assets/cocacola2lt.png",
+    "assets/coxinha.png",
+    "assets/pastel.png",
+    "assets/hotdog.png",
+    "assets/pasteldeforno.png",
+    "assets/batatafrita.png",
+    "assets/batatafritaturbinada.png",
+    // Adicione todos os caminhos para as suas imagens aqui
+];
+
+// Função para pré-carregar as imagens
+function preloadImagens() {
+    for (var i = 0; i < imagens.length; i++) {
+        var img = new Image();
+        img.src = imagens[i];
+    }
+}
+
+// Chame a função de pré-carregamento
+preloadImagens();
+
+
+
